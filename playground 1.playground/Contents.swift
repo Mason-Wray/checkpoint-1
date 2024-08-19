7f055ac75ad4f268301e989c664f2e4bb396f62e
MasonWray <mason.wray@stu.barren.kyschools.us>
2024-08-19 09:23:17 -0500

    checkpoint1 completed

---
+++ playground 1.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+let celsius = 15
+let fahrenheit = celsius*9/5+32
+var Ctemp = "The temperature is \(celsius) degrees celsius!"
+var Ftemp = "The temperature is \(fahrenheit) degrees fahrenheit!"
+print(Ctemp);
+print(Ftemp)
